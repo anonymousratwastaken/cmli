d653f0d2b6400e9b722ae01fcd5ebbeb68c0fad4
anonymousratwastaken <108170934+anonymousratwastaken@users.noreply.github.com>
2024-12-23 08:14:30 +0000

    Initial commit
    Added everything

---
+++ src/components/ui/listen-button.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+interface Props {
+  yt: any;
+  spotify: any;
+  itunes: any;
+}
+
+const Dropdown: React.FC<Props> = ({ yt, spotify, itunes }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="noShadow" className="max-w-24">
+          Listen
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuGroup>
+          <a href={itunes} className="cursor-pointer">
+          <DropdownMenuItem>Apple Music</DropdownMenuItem>
+          </a>
+          <a href={yt} className="cursor-pointer">
+          <DropdownMenuItem>YouTube Music</DropdownMenuItem>
+          </a>
+          <a href={spotify} className="cursor-pointer">
+          <DropdownMenuItem>Spotify</DropdownMenuItem>
+          </a>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Dropdown;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,46 +1,87 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		fontFamily: {
-			sans: ['Lexend', 'Inter', 'sans-serif'],
-			peta: ['Lexend Peta', 'Lexend', 'sans-serif'],
-		},
-    	extend: {
-    		colors: {
-    			main: '#f1ff5b',
-    			mainAccent: '#e8ff66',
-    			overlay: 'rgba(0,0,0,0.8)',
-    			bg: '#f2f2df',
-				shadedBg: '#ededc8',
-    			text: '#000',
-    			border: '#000',
-    			darkBg: '#333227',
-    			darkText: '#eeefe9',
-    			darkBorder: '#000',
-    			secondaryBlack: '#212121'
-    		},
-    		borderRadius: {
-    			base: '12px',
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		boxShadow: {
-    			light: '0px 4px 0px 0px #000',
-    			dark: '0px 4px 0px 0px #000'
-    		},
-    		translate: {
-    			boxShadowX: '0px',
-    			boxShadowY: '4px',
-    			reverseBoxShadowX: '0px',
-    			reverseBoxShadowY: '-4px'
-    		},
-    		fontWeight: {
-    			base: '500',
-    			heading: '700'
-    		}
-    	}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/pages/404.astro",
+    "./public/**/*.svg",
+  ],
+  safelist: ["fill-primary"],
+  theme: {
+    fontFamily: {
+      sans: ["Figtree", "Inter", "sans-serif"],
+      mono: ["Victor Mono", "monospace"],
+      varWidth: [
+        "--apple-system",
+        "BlinkMacSystemFont",
+        "Roboto Flex",
+        "Figtree",
+        "Inter",
+        "sans-serif",
+      ],
     },
-	plugins: [require("tailwindcss-animate")],
-}
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        base: "12px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      boxShadow: {
+        light: "0px 4px 0px 0px #000",
+        dark: "0px 4px 0px 0px #000",
+      },
+      translate: {
+        boxShadowX: "0px",
+        boxShadowY: "4px",
+        reverseBoxShadowX: "0px",
+        reverseBoxShadowY: "-4px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
