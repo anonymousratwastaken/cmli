eb55821f803d1c93a6d75052a2ea27ae90f04375
anonymousratwastaken <108170934+anonymousratwastaken@users.noreply.github.com>
2024-12-23 09:21:11 +0000

    Add Vercel adapter and update playlists page title

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno(),
+  adapter: vercel(),
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react(), mdx()]
