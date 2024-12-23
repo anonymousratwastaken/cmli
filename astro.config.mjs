73b966ef4840a9d285eff00172806a4803b87705
anonymousratwastaken <108170934+anonymousratwastaken@users.noreply.github.com>
2024-12-23 09:29:31 +0000

    We cant use vercel loll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,10 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: deno(),
   integrations: [tailwind({
     applyBaseStyles: false,
   }), react(), mdx()]
