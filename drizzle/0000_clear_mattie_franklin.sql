654a4265155debc119525947d9ff01002b93db49
anonymousratwastaken <108170934+anonymousratwastaken@users.noreply.github.com>
2025-03-05 16:59:44 +0000

    Implement SingleStore integration with drizzle ORM; update search functionality and configuration

---
+++ drizzle/0000_clear_mattie_franklin.sql
@@ -0,0 +1,14 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE `cmli` (
+	`name` text,
+	`artist` text,
+	`lyrics` text,
+	`copyright` text,
+	`musicbrainz` text,
+	`img` text,
+	`uuid` text
+);
+
+*/
---
+++ drizzle/schema.ts
@@ -0,0 +1,12 @@
+import { singlestoreTable, singlestoreSchema, AnySingleStoreColumn, text } from "drizzle-orm/singlestore-core"
+import { sql } from "drizzle-orm"
+
+export const cmli = singlestoreTable("cmli", {
+	name: text(),
+	artist: text(),
+	lyrics: text(),
+	copyright: text(),
+	musicbrainz: text(),
+	img: text(),
+	uuid: text(),
+});
